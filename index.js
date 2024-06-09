e3ac4559ff22de4caa7fc112412fd32eb3e448d6
martinaFCovaro <mfernandezcovaro@udesa.edu.ar>
2024-06-09 16:22:45 -0300

    segui editando el index.js

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 fetch('https://fakestoreapi.com/products')
-    .then(function(response) {
+    .then(function (response) {
         return response.json();
-        
+
     })
-    .then(function(data){
-        console.log(data)
+    .then(function (data) {
+        console.log(data.results)
+        let prueba = document.querySelector('.prueba')
+        let productos = "";
         for (let i = 0; i < data.length; i++) {
-            document.querySelector('.articulos-home').innerHTML + Image
-            
+            let producto = data[i];
+            productos +=
+                `
+        <article>
+        <img src="${producto.image}" alt="${producto.title}"></img>
+        <p>${producto.title}</p>
+        <p> $${producto.price}</p>
+        </article>
+        `;
         }
-
-
+        prueba.innerHTML = productos;
     })
-    .catch(function(error){
+    .catch(function (error) {
         console.log("El error es: " + error);
     })
+
+
+let botonAddToCart = document.querySelector('.boton-carrito');
+
+botonAddToCart.addEventListener('click', function(){
+
+
+})
