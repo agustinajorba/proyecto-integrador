cac3710494acb402a20799c0750939650e7b5a94
martinaFCovaro <mfernandezcovaro@udesa.edu.ar>
2024-06-11 13:36:29 -0300

    agregue de indes.js

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ fetch('https://fakestoreapi.com/products')
 
     })
     .then(function (data) {
-        console.log(data)
-        let contenedor = document.querySelector('.contenedor')
-        let productos = "";
+        console.log(data);
+
+        let contenedor = document.querySelector('.contenedor');
+        let productos = [];
         for (let i = 0; i < data.length; i++) {
             let producto = data[i];
             productos +=
@@ -14,16 +15,16 @@ fetch('https://fakestoreapi.com/products')
         <article class='contenedor contenedor-producto '>
         <img src="${producto.image}" alt="${producto.title}" class= "foto-producto"></img>
         <p class= 'textos-secciones'>${producto.title}</p>
-        <p class='ver-mas textos-secciones' data-producto-id="${producto.id}">Ver Más</p>
-        <p class='textos-precios textos-secciones'> ${producto.price}</p>
+        <a href="./producto.html?id=${producto.id}" class='ver-mas textos-secciones'>Ver Más</a>
+        <p class='textos-precios textos-secciones'> $${producto.price}</p>
         </article>
         `;
         }
         contenedor.innerHTML = productos;
-       
+
 
     })
-    
+
     .catch(function (error) {
         console.log("El error es: " + error);
     })
